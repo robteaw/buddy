ee16800ba97b2a5310fcdd7e3a9c5b311d28a640
Robert Tea <robteaw@gmail.com>
2023-09-14 08:21:25 -0500

    3. Login Updated

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -5,8 +5,23 @@ function Login() {
   return (
     <div>
       <h1>Login</h1>
+      
       <form action="">
-        <input type="text" name="name" id="name" placeholder="name" required />
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          required
+        />
+        <label htmlFor="password">Password</label>
+        <input type="text" name="password" id="password" required/>
+        <p><input type="checkbox" />Remember Me <a href="">Forget password?</a></p>
+        <input type="submit" value="Sign In" />
+        <p>New User? <a href="">Sign Up</a></p>
+        <p>OR</p>
+        <p>Sign in with Google</p>
+        <p>Sign in with Facebook</p>
       </form>
     </div>
   );

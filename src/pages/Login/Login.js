df61a14b5829c321fd86cdf27102f6fd04e034a2
robteaw <robteaw@gmail.com>
2023-09-13 23:55:20 -0500

    2. Login Page Added

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Login.css";
+
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form action="">
+        <input type="text" name="name" id="name" placeholder="name" required />
+      </form>
+    </div>
+  );
+}
+
+export default Login;
